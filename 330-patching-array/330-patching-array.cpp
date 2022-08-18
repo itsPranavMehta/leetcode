63c325444def0259c3f4f8e7335900c11a8ad4a5
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-18 23:17:15 +0530

    Time: 8 ms (76.69%), Space: 11.4 MB (9.82%) - LeetHub

---
+++ 330-patching-array/330-patching-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long missing=1;
+        int added=0,idx=0;
+        while(missing<=n){
+            if(idx<nums.size() && nums[idx]<=missing){
+                missing+=nums[idx++];
+            }
+            else{
+                missing+=missing;
+                added++;
+            }
+            
+        }
+        return added;
+    }
+};
