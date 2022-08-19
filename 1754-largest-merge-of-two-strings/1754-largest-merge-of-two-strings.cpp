e17e43fe8f0cad9e11f78133617a0a4914802a9a
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-19 22:59:44 +0530

    Time: 509 ms (26.39%), Space: 401 MB (23.61%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
@@ -6,12 +6,13 @@ public:
         int i=0,j=0,n=word1.size(),m=word2.size();
         string smallestString;
         while(i<n && j<m){
-            if(word1.compare(i,word1.size()-i,word2,j,word2.size()-j)>0){
+            // if(word1.compare(i,word1.size()-i,word2,j,word2.size()-j)>0){
+            if(word1.substr(i)>word2.substr(j)){
                 smallestString.push_back(word1[i++]);
             }
             else{
                 smallestString.push_back(word2[j++]);
-            } 
+            }
         }
         
         if(i<n) smallestString+=word1.substr(i);
