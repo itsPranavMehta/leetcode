d22b1f4ef8fb6ac67b5815ecd7282b66ae4a901a
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-20 18:51:44 +0530

    Time: 2 ms (57.41%), Space: 6 MB (99.26%) - LeetHub

---
+++ 1694-reformat-phone-number/1694-reformat-phone-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reformatNumber(string number) {
+        string answer;
+        for(auto& num:number){
+            if(num>='0' && num<='9'){
+                answer.push_back(num);
+            }
+        }
+        int i=3;
+        if(answer.size()<4) return answer;
+        if(answer.size()<5) i=2;
+        while((i<=(int)answer.size()-6) || (i==(int)answer.size()-5) || (i==(int)answer.size()-3)){
+            answer.insert(i,1,'-');
+            i+=4;
+        }
+        while(i<=(int)answer.size()-2){
+            answer.insert(i,1,'-');
+            i+=3;
+        }
+        return answer;
+        
+    }
+};
