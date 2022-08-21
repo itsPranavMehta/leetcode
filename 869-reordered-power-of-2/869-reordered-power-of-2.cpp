8b3dda85f1bffd8895e0fd03ca8710c943bd83b7
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-21 22:44:03 +0530

    Time: 3 ms (73.16%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    bool isAnagram(int n,int x){
+    bool isAnagram(int n,int x){        //return true if anagram
         vector<int> vec(10,0);
         while(n){
             vec[n%10]++;
@@ -14,7 +14,8 @@ private:
         for(auto& i:vec)    if(i!=0)    return 0;
         return 1;
     }
-    int countDigs(int n){
+    
+    int countDigs(int n){               //return number of digits
         int count=0;
         while(n){
             n/=10;
@@ -22,7 +23,8 @@ private:
         }
         return count;
     }
-    int smallestNumber(int n){
+    
+    int smallestNumber(int n){          //smallest power of 2 with n digits
         int l=0,r=31;
         int best_ans=0;
         while(l<=r){
@@ -41,6 +43,7 @@ private:
 public:
     bool reorderedPowerOf2(int n) {
         // so the power of 2 for any digit (say n) can at max be 4.. hence i'll calculate all the power of 2s in that many digit and would check if it's possible to make that number by rearranging those..
+        
         int smallest_valid=smallestNumber(countDigs(n));
         while(countDigs(smallest_valid)==countDigs(n)){
             if(isAnagram(n,smallest_valid)) return 1;
