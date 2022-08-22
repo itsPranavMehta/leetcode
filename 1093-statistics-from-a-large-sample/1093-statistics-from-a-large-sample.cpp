fa8fde1fc154170de038e83fb3ae4aac13c0a719
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-22 23:37:35 +0530

    Time: 7 ms (50.63%), Space: 8.4 MB (67.09%) - LeetHub

---
+++ 1093-statistics-from-a-large-sample/1093-statistics-from-a-large-sample.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<double> sampleStats(vector<int>& count) {
+        double mn=-1,mx,mean,median=-1,mode;
+        int readings=accumulate(count.begin(),count.end(),0);
+        long sum=0;
+        int mostOccured=0;
+        for(int idx=0,numNonZero=0;idx<count.size();idx++){
+            int c=count[idx];
+            if(c!=0){
+                if(mn==-1)  mn=idx;
+                mx=idx;
+                sum+=(0ll+idx)*c;
+                if(mostOccured<c){
+                    mostOccured=c;
+                    mode=idx;
+                }
+                if(c*2+numNonZero>=readings && median==-1){
+                    if(c*2+numNonZero==readings)    median=(idx+0.0)/2;
+                    else median=idx;
+                }
+                else if(numNonZero==readings){
+                    median+=(idx+0.0)/2;
+                }
+                numNonZero+=2*c;
+            }
+        }
+        mean=(sum+0.0)/readings;
+        
+        
+        return {mn,mx,mean,median,mode};
+        
+    }
+};
