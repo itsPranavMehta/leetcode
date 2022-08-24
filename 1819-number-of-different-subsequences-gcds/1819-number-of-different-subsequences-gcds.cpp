a5c06d09741704237c5b569e7630d16efd4444dc
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-24 20:29:51 +0530

    Time: 657 ms (43.59%), Space: 72 MB (82.69%) - LeetHub

--- 1819-number-of-different-subsequences-gcds/1819-number-of-different-subsequences-gcds.cpp
+++ 1819-number-of-different-subsequences-gcds/1819-number-of-different-subsequences-gcds.cpp
@@ -3,16 +3,17 @@ public:
     int countDifferentSubsequenceGCDs(vector<int>& nums) {
         
         int mx=*max_element(nums.begin(),nums.end());
-        vector<bool> elements(mx+1,0);
+        vector<bool> elements(mx+1,0);          // element lookup in o(1)
         for(auto& el:nums)  elements[el]=1;
+        
         int numGcds=0;
-        for(int num=1;num<=mx;num++)
+        for(int num=1;num<=mx;num++)            // checking for all possible gcds
         {
             int g=0;
-            for(int curr=num;curr<=mx;curr+=num){
+            for(int curr=num;curr<=mx;curr+=num){       // check for all elements that are a multiple of the current number
                 if(elements[curr])    g=gcd(g,curr);
-            }
-            if(g==num)  numGcds++;
+            }   
+            if(g==num)  numGcds++;              // this gcd can be constructed!
         }
         return numGcds;
     }
