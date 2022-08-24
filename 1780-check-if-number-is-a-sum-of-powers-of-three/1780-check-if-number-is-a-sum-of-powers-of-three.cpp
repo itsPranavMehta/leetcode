321b8594da2baf46f6d304348710a284403dee08
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-24 18:28:25 +0530

    Time: 2 ms (53.97%), Space: 5.9 MB (77.62%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n,int p=0,int idx=0){
+        // the ternary representation can contain 0-> this power not included, 1-> this power included,but can't contain, 2->this power included 2 times.
         while(n){
             if(n%3==2)  return 0;
             n=n%3==1?n-1:n/3;
