3efa8fa2f14e937d6fa3e277001d194b2de050c4
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-27 11:08:29 +0530

    Time: 3 ms (72.76%), Space: 6 MB (71.21%) - LeetHub

--- 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.cpp
+++ 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int reinitializePermutation(int n) {
+        //so the pattern is that till the element is present at a loc less than n/2 then it doubles in every iteration, after that it's pace of increasing slows down until it reaches the end then it starts decreasing rapidly and this decreases slowly lessens. so since 1 is the first element which changes position (0 doesn't move at all) hence it becomes easy to track just once as once it has completed it's trajectory (which is the biggest) all other elements have also completed the same!
+        
         int ops=0,idx=1;
         while(ops==0 || idx>1){
             if(idx<n/2){
