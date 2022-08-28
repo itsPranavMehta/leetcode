e054e15ebe3aca0f2ed55c98a3c56e7f67bef6bf
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-08-28 10:03:54 +0530

    Time: 14 ms (63.06%), Space: 12.8 MB (91.79%) - LeetHub

--- 517-super-washing-machines/517-super-washing-machines.cpp
+++ 517-super-washing-machines/517-super-washing-machines.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findMinMoves(vector<int>& machines) {
+        
+        // so here we try to get the point where the maximum shifting would take place that is at what machine the number of clothes on left+ on current machine is most different than the number of clothes that optimally should've been there, these number of clothes would need to be transferred to the right and would hence require "number of clothes to be transferred time".
+        
         int n=machines.size();
         long sum=accumulate(machines.begin(),machines.end(),0ll);
         
