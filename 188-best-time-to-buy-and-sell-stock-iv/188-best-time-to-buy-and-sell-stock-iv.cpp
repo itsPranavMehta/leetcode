6f7571171fa05579e53639883a97e9e2f2c2fca8
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-09-10 22:51:04 +0530

    Time: 12 ms (75.26%), Space: 11.6 MB (69.81%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
+        
         int nothingInHand[n+1][k+1],stockBought[n+1][k+1];
+
         memset(nothingInHand,0,sizeof nothingInHand);
         memset(stockBought,0,sizeof stockBought); 
-        for(int i=0;i<=k;i++)   stockBought[0][i]=INT_MIN;
+        
+        for(int i=0;i<=k;i++)   stockBought[0][i]=INT_MIN;      //just a random max value.
+        
         for(int i=1;i<=n;i++)
         {
-            
             for(int j=1;j<=k;j++)
             {
                 nothingInHand[i][j]=max(nothingInHand[i-1][j],stockBought[i-1][j]+prices[i-1]);
                 stockBought[i][j]=max(stockBought[i-1][j],nothingInHand[i-1][j-1]-prices[i-1]);
-                
             }
         }
         
