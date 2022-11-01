43b62d70ea1709f8634cfee03522f633800a1302
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2022-11-01 20:23:53 +0530

    Time: 52 ms (38.46%), Space: 11.8 MB (56.09%) - LeetHub

--- 1139-largest-1-bordered-square/1139-largest-1-bordered-square.cpp
+++ 1139-largest-1-bordered-square/1139-largest-1-bordered-square.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int largest1BorderedSquare(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
-        vector<vector<int>> left(n,vector<int>(m,0));
-        vector<vector<int>> up(n,vector<int>(m,0));
+        vector<vector<int>> left(n,vector<int>(m,0)), up(n,vector<int>(m,0));
+        
         for(int i=0;i<n;i++){
             for(int j=0,score=0;j<m;j++){
                 if(grid[i][j]==0)   score=0;
@@ -11,6 +11,7 @@ public:
                 left[i][j]=score;
             }
         }
+        
         for(int i=0;i<m;i++){
             for(int j=0,score=0;j<n;j++){
                 if(grid[j][i]==0)   score=0;
@@ -26,9 +27,10 @@ public:
                 int maxPossible=min(left[i][j],up[i][j]);
                 for(int dis=maxPossible-1;dis>=0;dis--){    // -1 because self is included.         
                     int x=i-dis,y=j-dis;
-                    int currMax=min(left[x][j],up[i][y]);
+                    int currMax=min(left[x][j],up[i][y]);   
+                    
                     if(currMax>dis){                        // only then a square can be formed.
-                        best=max(best,(dis+1)*(dis+1));
+                        best=max(best,(dis+1)*(dis+1));     // dis represent the distance of the point under consideration from (i,j) since both the point under consideration and i,j are to be used for the square hence the area is (dis+1)*(dis+1)
                     }
                     
                 }
