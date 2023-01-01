c3cd19bbf5cc734117ba8bd2ca14a6dca7e17ef0
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2023-01-01 12:44:50 +0530

    Added solution - LeetHub

--- Count even length - GFG/count-even-length.cpp
+++ Count even length - GFG/count-even-length.cpp
@@ -23,7 +23,7 @@ private:
 	int compute_value(int n)
 	{
 	    int p=1e9+7,res=1;      // nc0 =1 so storing it
-	    int ncr=1;
+	    int ncr=1;  // nc0
 	    for(int i=1;i<=n;i++){// ncr =(( n+1-r)/r)*nC(r-1)
 	        ncr=(((1ll*(n-i+1)*ncr)%p)*modIn(i))%p;
 	        res=(res+1ll*ncr*ncr)%p;
