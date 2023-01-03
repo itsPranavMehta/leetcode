c6a8cb84147b4d33130c0f266669ff8aa2538cfd
itsPranavMehta <104422766+itsPranavMehta@users.noreply.github.com>
2023-01-04 00:45:15 +0530

    Added solution - LeetHub

--- Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
+++ Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
@@ -9,19 +9,29 @@ using namespace std;
 class Solution {
   public:
     int maximum_profit(int n, vector<vector<int>> &intervals) {
-        // Write your code here.
+        // sort -> 
+        // 1st element of the arrray ! 
         sort(intervals.begin(),intervals.end(),[](vector<int>& a,vector<int>& b){
             return a[1]!=b[1]?a[1]<b[1]:(a[0]!=b[0]?a[0]<b[0]:a[2]>b[2]);
         });
-        map<int,int> profits;
-        profits[-1]=0;
+        
+        map<int,int> profits;       // sorted ,,, 
+        // lower bound and upper bound 
+        
+        // lowerbound(x) -> x or greater than x (smallest possible val)
+        // upperbound(x) -> greater than x.... 
+        
+        profits[-1]=0;      // time -1=0; /
+        
         for(auto& interval: intervals){
+            
             int s=interval[0],e=interval[1],p=interval[2];
+            
             int mxProfit=max((prev(profits.upper_bound(e)))->second,
             (prev(profits.upper_bound(s)))->second+p);
             profits[e]=mxProfit;
         }
-        return prev(profits.end())->second;
+        return profits.rbegin()->second;
     }
 };
 
